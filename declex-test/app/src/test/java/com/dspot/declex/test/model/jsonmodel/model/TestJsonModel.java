bc63cc23d4569b9440f35901602ba6bc61931281
Diovi Robinet Morales <drobinetm@outlook.com>
2017-11-03 18:23:28 -0500

    Testing to get the JsonModel annotation model in json format

--- declex-test/app/src/test/java/com/dspot/declex/test/model/jsonmodel/model/TestJsonModel.java
+++ declex-test/app/src/test/java/com/dspot/declex/test/model/jsonmodel/model/TestJsonModel.java
@@ -102,4 +102,30 @@ public class TestJsonModel {
             assertEquals(true, workerSocialDeserialize.isStudyUniversity());
         }
     }
+
+    @Test
+    public void testConvertToJsonModel() {
+        final String modelJson = "{\"work_place\":\"Schools\",\"professional_functions\":\"Supervision\",\"study_university\":true,\"address\":null}";
+
+        ModelSocialWorker_ socialWorker = new ModelSocialWorker_();
+        socialWorker.setWorkPlace("Schools");
+        socialWorker.setProfessionalFunctions("Supervision");
+        socialWorker.setStudyUniversity(true);
+
+        String jsonGenerated = socialWorker.toJson();
+        assertEquals(modelJson, jsonGenerated);
+    }
+
+    @Test
+    public void testConvertToJsonModelSomeAttributes() {
+        final String modelJson = "{\"work_place\":\"Schools\",\"professional_functions\":\"Supervision\"}";
+
+        ModelSocialWorker_ socialWorker = new ModelSocialWorker_();
+        socialWorker.setWorkPlace("Schools");
+        socialWorker.setProfessionalFunctions("Supervision");
+        socialWorker.setStudyUniversity(true);
+
+        String jsonGenerated = socialWorker.toJson("work_place, professional_functions");
+        assertEquals(modelJson, jsonGenerated);
+    }
 }
